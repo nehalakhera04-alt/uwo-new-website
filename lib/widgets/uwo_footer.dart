afd19b7f543df9154fa3c003766e36a19cae0207
Aditilakhera <aditilakhera0@gmail.com>
2025-12-20 11:42:41 +0530

    Fix

--- lib/widgets/uwo_footer.dart
+++ lib/widgets/uwo_footer.dart
@@ -1,23 +1,3 @@
-// import 'package:flutter/material.dart';
-// import '../theme/app_theme.dart';
-//
-// class UWOFooter extends StatelessWidget {
-//   const UWOFooter({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       padding: const EdgeInsets.all(24),
-//       color: AppTheme.softBlue,
-//       child: const Center(
-//         child: Text(
-//           'Unified Web Options & Services Pvt. Ltd.',
-//           style: TextStyle(color: AppTheme.textMuted),
-//         ),
-//       ),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 
 class UWOFooter extends StatelessWidget {
