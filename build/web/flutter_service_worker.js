2992e0510f58e91ef1e95cb4ed2dc777c2b2089d
Aditilakhera <aditilakhera0@gmail.com>
2025-12-20 12:02:17 +0530

    Fix

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -36,7 +36,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "381598babd6bcadf5bd21cca9878a1e0
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "3093e83dbc4d70d46613c2b70d2fc9f5",
+"flutter_bootstrap.js": "180fd659ff3986a42be7f548d5c57a60",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
